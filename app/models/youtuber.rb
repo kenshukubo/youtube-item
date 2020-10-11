345f72ce33413176ebd3b98ba31c5942d180c000
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-12 00:30:46 +0900

    enum

--- app/models/youtuber.rb
+++ app/models/youtuber.rb
@@ -2,4 +2,7 @@ class Youtuber < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true
+
+  enum sex:       {men: 1, women: 2}
+  enum category:  {youtuber: 1, actor: 2, actress: 3, model: 4, entertainer: 5}
 end
