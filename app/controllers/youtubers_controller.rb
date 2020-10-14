fae026e547383a324637f86c70506858984adf71
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-14 13:05:50 +0900

    fix

--- app/controllers/youtubers_controller.rb
+++ app/controllers/youtubers_controller.rb
@@ -1,5 +1,20 @@
+#require 'google/apis/youtube_v3' #YoutubeV3を使用するために、呼び出す
 class YoutubersController < ApplicationController
+  
   def index
-    
+    # youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    # youtube.key = ENV["YOUTUBE_APIKEY"]
+
+    # youtubers = Youtuber.all
+    # youtubers.find_each do |youtuber|
+    # end
+    # options = {
+    #   :channel_id => 'UCkMs-Zr9pGixbqm0mhO8J7g' #YouTubeチャンネルのIDを指定
+    # }
+
+    # response = youtube.list_searches("snippet", options)
+
+    # @channnel_icon = response.items[0].snippet.thumbnails.medium.url
   end
+
 end
