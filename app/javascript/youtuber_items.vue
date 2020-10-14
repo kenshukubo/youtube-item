cb00d2fa90025faf3f3d2a1810c6ec755fcc8ec2
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-14 23:17:00 +0900

    fix

--- app/javascript/youtuber_items.vue
+++ app/javascript/youtuber_items.vue
@@ -1,28 +1,25 @@
 <template>
   <div>
-    <h2>新着アイテム</h2>
-    <div>
-      <div v-for="(post, index) in posts" :key="index" class="post-content">
-        <h3>{{post.title}}</h3>
-        <div class="post-info">
-          <img :src="post.thumbnail_url">
-          <div class="post-detail">
-            <p>紹介商品数：{{post.items_num}}</p>
-          </div>
+    <div v-for="(post, index) in posts" :key="index" class="post-content">
+      <h3>{{post.title}}</h3>
+      <div class="post-info">
+        <img :src="post.thumbnail_url">
+        <div class="post-detail">
+          <p>紹介商品数：{{post.items_num}}</p>
         </div>
-        <div v-if="isActive !== index" @click="active(index)" class="item-list-btn">アイテム一覧を表示</div>
+      </div>
+      <div v-if="isActive !== index" @click="active(index)" class="item-list-btn">アイテム一覧を表示</div>
 
-        <div :class="{'is-active' : isActive === index}" class="item-list-switch">
-          <div v-for="(item, index) in post.items" :key="index" class="item-content">
-            <img :src="item.image.thumb_mini.url">
-            <div class="item-info">
-              <h3 class="item-name">{{item.name}}</h3>
-              <p class="item-price">{{item.price}}円<span class="tax-include">(税込)</span></p>
-              <ul>
-                <li><a :href="item.rakuten_url" class="item-link" target="_blank">楽天で詳細をみる</a></li>
-                <li><a :href="item.amazon_url" class="item-link" target="_blank">Amazonで詳細をみる</a></li>
-              </ul>
-            </div>
+      <div :class="{'is-active' : isActive === index}" class="item-list-switch">
+        <div v-for="(item, index) in post.items" :key="index" class="item-content">
+          <img :src="item.image.thumb_mini.url">
+          <div class="item-info">
+            <h3 class="item-name">{{item.name}}</h3>
+            <p class="item-price">{{item.price}}円<span class="tax-include">(税込)</span></p>
+            <ul>
+              <li><a :href="item.rakuten_url" class="item-link" target="_blank">楽天で詳細をみる</a></li>
+              <li><a :href="item.amazon_url" class="item-link" target="_blank">Amazonで詳細をみる</a></li>
+            </ul>
           </div>
         </div>
       </div>
