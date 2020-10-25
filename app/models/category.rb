7f6be1964328ebd04a7311837db7af4bafe74b05
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-25 17:42:01 +0900

    カテゴリーimage uploader

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_ancestry
   has_many :item_categories, dependent: :destroy
   has_many :items, through: :item_categories
+
+  mount_uploader :image, CategoryImageUploader
+
 end
