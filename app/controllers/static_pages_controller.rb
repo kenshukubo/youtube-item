d03f966b276b3c4e985d9fa36befac4da4eaeedb
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-25 18:02:31 +0900

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @youtubers = Youtuber.all.order(id: :desc)
     @posts = Post.all.order(id: :desc)
+    @categories = Category.where(ancestry: nil)
   end
 end
