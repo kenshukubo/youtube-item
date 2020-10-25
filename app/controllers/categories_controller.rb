a8487fb96d7b39db3d70461a8e22d34443d6b19a
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-26 00:11:47 +0900

    カテゴリーあいてむ

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,12 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find_by(params[:id])
+    @category = Category.find(params[:id])
     @categories = Category.all
+
+    @items = []
+    item_categories = ItemCategory.where(category_id: params[:id])
+    item_categories.find_each do |item_category|
+      @items << Item.find(item_category.item_id)
+    end
   end
 end
