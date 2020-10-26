75b91e44f856adde88ba5c67f9839f2e9cc46128
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-10-26 10:29:43 +0900

    fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
-
     @items = Item.includes(:item_categories).where(item_categories: {category_id: params[:id]})
-    # item_categories = ItemCategory.where(category_id: params[:id])
-    # item_categories.find_each do |item_category|
-    #   @items << Item.find(item_category.item_id)
-    # end
   end
 end
