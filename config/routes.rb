9753e3a409504e15d7b4fc339fa6ee7d0e208363
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-05 01:56:25 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :youtubers, only: [:show]
-      resources :searches, only: [:index]
     end
   end
 end
