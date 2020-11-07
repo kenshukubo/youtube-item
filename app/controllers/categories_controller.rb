1d5402828f09128f9a64d3e90539e68e7d8e276f
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-07 15:06:50 +0900

    category showにページャー

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
-    @items = Item.includes(:item_categories).where(item_categories: {category_id: params[:id]})
+    @items = Item.includes(:item_categories).where(item_categories: {category_id: params[:id]}).page(params[:page])
   end
 end
