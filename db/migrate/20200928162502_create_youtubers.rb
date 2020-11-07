4e731bfec6ba3c250c269ce1981d675182b0985f
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-07 15:16:03 +0900

    fix

--- db/migrate/20200928162502_create_youtubers.rb
+++ db/migrate/20200928162502_create_youtubers.rb
@@ -1,12 +1,11 @@
 class CreateYoutubers < ActiveRecord::Migration[6.0]
   def change
     create_table :youtubers do |t|
-
-      t.timestamps
       t.string "name"
       t.integer "sex"
       t.integer "category"
       t.string "image"
+      t.timestamps
     end
   end
 end
