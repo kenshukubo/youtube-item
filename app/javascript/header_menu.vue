37f3b30516fe1cd895e8213848f1fcc0219e1699
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-08 12:06:25 +0900

    ずらし

--- app/javascript/header_menu.vue
+++ app/javascript/header_menu.vue
@@ -5,33 +5,33 @@
     </div>
 
     <div v-if="isMenuOpen" class="menu-wrapper">
-      <nav class="menu">
-        <div class="close-btn">
-          <img :src="closeBtn" @click="closeMenu">
-        </div>
-        <div class="search-form-wrapper">
-          <input class="search-form" v-model="text" type="text">
-          <button class="search-btn" @click="search">検索</button>
-        </div>
-        <ul>
-          <li v-for="(category, index) in categories" :key="index" class="category-wrapper">
-            <a :href="`/categories/${category.id}`" class="category-item">
-              <img :src="category.image">
-              <p>{{category.name}}</p>
-            </a>
-          </li>
-          <li v-for="(category, index) in children_categories" :key="index" class="category-wrapper">
-            <a :href="`/categories/${category.id}`" class="category-item">
-              <img :src="category.image">
-              <p>{{category.name}}</p>
-            </a>
-          </li>
-        </ul>
-        <div class="bottom-close-area" @click="closeMenu">
-          <p class="bottom-close-btn">閉じる</p>
-        </div>
-      </nav>
     </div>
+    <nav class="menu" v-if="isMenuOpen">
+      <div class="close-btn">
+        <img :src="closeBtn" @click="closeMenu">
+      </div>
+      <div class="search-form-wrapper">
+        <input class="search-form" v-model="text" type="text">
+        <button class="search-btn" @click="search">検索</button>
+      </div>
+      <ul>
+        <li v-for="(category, index) in categories" :key="index" class="category-wrapper">
+          <a :href="`/categories/${category.id}`" class="category-item">
+            <img :src="category.image">
+            <p>{{category.name}}</p>
+          </a>
+        </li>
+        <li v-for="(category, index) in children_categories" :key="index" class="category-wrapper">
+          <a :href="`/categories/${category.id}`" class="category-item">
+            <img :src="category.image">
+            <p>{{category.name}}</p>
+          </a>
+        </li>
+      </ul>
+      <div class="bottom-close-area" @click="closeMenu">
+        <p class="bottom-close-btn">閉じる</p>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -89,65 +89,68 @@ export default {
   background: rgba(45,45,45, 0.5);
   z-index: 99;
   overflow: auto;
-  .menu{
-    width: 300px;
-    height: 100%;
-    background-color: #fff;
-    margin-left: auto;
-    overflow: scroll;
-    z-index: 100;
-    .close-btn{
-      height: 70px;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      padding-right: 30px;
-      img{
-        width: 32px;
-        height: 32px;
-      }
+}
+.menu{
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 300px;
+  height: 100%;
+  background-color: #fff;
+  margin-left: auto;
+  overflow: scroll;
+  z-index: 100;
+  .close-btn{
+    height: 70px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 30px;
+    img{
+      width: 32px;
+      height: 32px;
     }
-    .search-form-wrapper{
-      position: relative;
-      padding: 15px;
-      margin-bottom: 10px;
-      .search-form{
-        width: 100%;
-        border-radius: 30px;
-        border: 1px solid #e7e7e7;
-        padding: 10px 16px;
-        box-sizing: border-box;
-      }
-      .search-btn{
-        border-radius: 28px;
-        background-color: #333;
-        color: #fff;
-        font-size: 13px;
-        font-weight: bold;
-        height: 30px;
-        padding: 0 10px;
-        position: absolute;
-        right: 20px;
-        top: 18px;
-      }
-    }
-    .category-wrapper{
-      padding-left: 24px;
-      margin-bottom: 10px;
-      .category-item{
-        display: flex;
-        align-items: center;
-        font-weight: 400;
-        padding: 14px 0;
-      }
-    }
-    .bottom-close-area{
+  }
+  .search-form-wrapper{
+    position: relative;
+    padding: 15px;
+    margin-bottom: 10px;
+    .search-form{
+      width: 100%;
+      border-radius: 30px;
       border: 1px solid #e7e7e7;
-      text-align: center;
-     .bottom-close-btn{
-       padding: 20px 0;
-     } 
+      padding: 10px 16px;
+      box-sizing: border-box;
     }
+    .search-btn{
+      border-radius: 28px;
+      background-color: #333;
+      color: #fff;
+      font-size: 13px;
+      font-weight: bold;
+      height: 30px;
+      padding: 0 10px;
+      position: absolute;
+      right: 20px;
+      top: 18px;
+    }
+  }
+  .category-wrapper{
+    padding-left: 24px;
+    margin-bottom: 10px;
+    .category-item{
+      display: flex;
+      align-items: center;
+      font-weight: 400;
+      padding: 14px 0;
+    }
+  }
+  .bottom-close-area{
+    border: 1px solid #e7e7e7;
+    text-align: center;
+    .bottom-close-btn{
+      padding: 20px 0;
+    } 
   }
 }
 </style>
