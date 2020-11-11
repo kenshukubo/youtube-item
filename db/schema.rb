645ca7c30eb58df978de6cc068f2ef414e6d8cdc
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-11 10:14:55 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -55,11 +55,11 @@ ActiveRecord::Schema.define(version: 2020_10_30_040722) do
   end
 
   create_table "youtubers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "category"
     t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "channel_id"
   end
 
