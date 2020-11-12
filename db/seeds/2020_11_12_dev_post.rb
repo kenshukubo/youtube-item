ecfba9322afb145ac13871ea9ed260765d60b741
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-12 14:24:47 +0900

    post second

--- db/seeds/2020_11_12_dev_post.rb
+++ db/seeds/2020_11_12_dev_post.rb
@@ -5,11 +5,6 @@ youtube = Google::Apis::YoutubeV3::YouTubeService.new
 youtube.key = ENV["YOUTUBE_APIKEY"]
 
 ids = [
-  ['UeBXdnHjhXE', 1, 1, 'morning_routin'],
-  ['47YnnAAh3o0', 2, 1, nil],
-  ['uLSE9dJcBVI', 3, 1, nil],
-  ['vTeUxlZapeM', 4, 2, 'home_time'],
-  ['4EAt82vGlFA', 5, 2, nil]
   ['YoI23FPPYvU', 6, 3, 'daily_make'],
   ['BVEccxsvOTI', 7, 3, nil],
   ['JvbP1b8vfcI', 8, 3, nil],
@@ -116,22 +111,20 @@ ids.each do |id|
   }
 
   response = youtube.list_videos("snippet", options)
-
+  p id[0]
   title = response.items[0].snippet.title
 
-  # url = response.items[0].snippet.thumbnails.high.url
-  # file = "#{Rails.root}/db/images/video_image/#{id[1]}.png"
-  # open(file, 'wb') do |pass|
-  #   open(url) do |recieve|
-  #     pass.write(recieve.read)
-  #   end
-  # end
-
-  date = 20201112
+  url = response.items[0].snippet.thumbnails.high.url
+  file = "#{Rails.root}/db/images/video_image/#{id[1]}.png"
+  open(file, 'wb') do |pass|
+    open(url) do |recieve|
+      pass.write(recieve.read)
+    end
+  end
 
   Post.create!(
     title: title, 
-    thumbnail: open("#{Rails.root}/db/#{date}/#{id[1]}.png"),
+    thumbnail: open("#{Rails.root}/db/images/video_image/#{id[1]}.png"),
     video_id: "#{id[0]}",
     youtuber_id: id[2],
     category: id[3]
