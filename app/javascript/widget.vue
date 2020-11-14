3aaa48a141c48063859abdbdac8058855b7a5b5a
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-14 10:44:16 +0900

    カテゴリー

--- app/javascript/widget.vue
+++ app/javascript/widget.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <h2 class="header-sidebar">カテゴリー</h2>
-    <div class="category-list" v-for="(category, index) in categories" :key="index">
+
+    <ul class="category-list" v-for="(category, index) in categories" :key="index">
+      <li class="first-category">
+        <a :href="`/categories/${category.id}`">
+          <p>{{category.name}}(すべて)</p>
+        </a>
+      </li>
       <ul>
-        <li>
-          <a>
-            <p>{{category.name}}</p>
+        <li v-for="(second_category, index) in category.second_categories" :key="index" @click="selectCategory(index)">
+          <a class="second-category">
+            <img :src="`/assets/images/arrow.png`" class="arrow-img">
+            <p>{{second_category.name}}</p>
           </a>
-        </li>
-        <ul v-for="(second_category, index) in category.second_categories" :key="index">
-          <li>
-            <a>
-              <p>{{second_category.name}}</p>
-            </a>
-          </li>
-          <ul v-for="(third_category, index) in second_category.third_categories" :key="index">
-            <li>
-              <a>
+          <ul v-if="isActive === index" class="drop-menu">
+            <li v-for="(third_category, index) in second_category.third_categories" :key="index">
+              <a :href="`/categories/${third_category.id}`" class="third-category" >
                 <p>{{third_category.name}}</p>
               </a>
             </li>
           </ul>
-        </ul>
+        </li>
       </ul>
-    </div>
+    </ul>
   </div>
 </template>
 <script>
@@ -32,7 +32,8 @@ import axios from 'packs/axios'
 export default {
   data: function () {
     return {
-      categories: []
+      categories: [],
+      isActive: ""
     }
   },
   created () {
@@ -47,7 +48,13 @@ export default {
     });
   },
   methods: {
-
+    selectCategory: function(i){
+      if(this.isActive === i){
+        this.isActive = null;
+      }else{
+        this.isActive = i;
+      }
+    },
   }
 }
 </script>
@@ -61,6 +68,38 @@ export default {
   margin-bottom: 10px;
 }
 .category-list{
-
+  .first-category{
+    padding: 8px 0;
+    margin-bottom: 8px;
+    cursor: pointer;
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+  .second-category{
+    display: flex;
+    align-items: center;
+    padding: 8px 0;
+    cursor: pointer;
+    &:hover{
+      text-decoration: underline;
+    }
+    .arrow-img{
+      width: 12px;
+      height: 12px;
+      margin-right: 10px;
+    }
+  }
+  .drop-menu{
+    padding-left: 20px;
+    .third-category{
+      display: block;
+      padding: 8px 0;
+      cursor: pointer;
+      &:hover{
+        text-decoration: underline;
+      }
+    }
+  }
 }
 </style>
--- app/views/api/v1/widget/index.json.jbuilder
+++ app/views/api/v1/widget/index.json.jbuilder
@@ -1,8 +1,10 @@
 json.categories do
   json.array!(@first_categories) do |category|
+    json.id   category.id
     json.name category.name
     json.second_categories do
       json.array! Category.where(ancestry: category.id) do |second_category|
+        json.id   second_category.id
         json.name second_category.name
         json.third_categories Category.where(ancestry: "#{category.id}/#{second_category.id}")
       end
