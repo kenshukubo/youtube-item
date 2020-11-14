81395817f5ebcf67a87a7e21128c65989163fff4
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-14 11:27:22 +0900

    プルダウン

--- app/javascript/widget.vue
+++ app/javascript/widget.vue
@@ -11,8 +11,9 @@
       <ul>
         <li v-for="(second_category, index) in category.second_categories" :key="index" @click="selectCategory(index)">
           <a class="second-category">
-            <img :src="`/assets/images/arrow.png`" class="arrow-img">
             <p>{{second_category.name}}</p>
+            <img :src="`/assets/images/pull-up.png`" v-if="isActive === index" class="arrow-img">
+            <img :src="`/assets/images/pull-down.png`" v-else class="arrow-img">
           </a>
           <ul v-if="isActive === index" class="drop-menu">
             <li v-for="(third_category, index) in second_category.third_categories" :key="index">
@@ -62,15 +63,17 @@ export default {
 .header-sidebar{
   border-left: 3px solid #02bb80;
   color: #313131;
-  font-size: 20px;
+  font-size: 24px;
   font-weight: bold;
   padding-left: 16px;
   margin-bottom: 10px;
 }
 .category-list{
+  font-size: 18px;
   .first-category{
     padding: 8px 0;
     margin-bottom: 8px;
+    font-weight: 600;
     cursor: pointer;
     &:hover{
       text-decoration: underline;
@@ -79,15 +82,15 @@ export default {
   .second-category{
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 8px 0;
     cursor: pointer;
     &:hover{
       text-decoration: underline;
     }
     .arrow-img{
-      width: 12px;
-      height: 12px;
-      margin-right: 10px;
+      width: 16px;
+      height: 16px;
     }
   }
   .drop-menu{
