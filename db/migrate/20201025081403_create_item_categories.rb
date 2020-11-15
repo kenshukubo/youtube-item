e1e7884bfef32f7def7f80064a38b573383fa0d4
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-15 23:37:54 +0900

    fix

--- db/migrate/20201025081403_create_item_categories.rb
+++ db/migrate/20201025081403_create_item_categories.rb
@@ -1,7 +1,7 @@
 class CreateItemCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :item_categories do |t|
-      t.references :item, type: :bigint, foreign_key: true, null:false
+      t.references :item, type: :bigint, foreign_key: true
       t.references :category, type: :bigint, foreign_key: true, null:false
       t.timestamps
     end
