8f6f28c67bd9e694a2f14239cdc1c4cdfa7f4295
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-15 23:40:39 +0900

    fix

--- db/migrate/20201025081403_create_item_categories.rb
+++ db/migrate/20201025081403_create_item_categories.rb
@@ -1,6 +1,7 @@
 class CreateItemCategories < ActiveRecord::Migration[6.0]
   def change
-    create_table :item_categories do |t|
+    create_table :post_item_categories do |t|
+      t.references :post, type: :bigint, foreign_key: true
       t.references :item, type: :bigint, foreign_key: true
       t.references :category, type: :bigint, foreign_key: true, null:false
       t.timestamps
--- db/migrate/20201114032255_change_item_categories_to_post_item_categories.rb
+++
@@ -1,5 +0,0 @@
-class ChangeItemCategoriesToPostItemCategories < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :item_categories, :post_item_categories
-  end
-end
