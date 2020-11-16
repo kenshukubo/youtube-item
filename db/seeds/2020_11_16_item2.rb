33b79bbc0cdc9c3978decaee2ecdf730ebf153ab
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-16 22:23:38 +0900

    seed

--- db/seeds/2020_11_16_item2.rb
+++ db/seeds/2020_11_16_item2.rb
@@ -1,50 +1,17 @@
-require 'paapi'
-require 'open-uri'
-@client = Paapi::Client.new(
-  access_key: ENV["AMAZON_APIKEY"],
-  secret_key: ENV["AMAZON_SECRETKEY"],
-  market: :jp,
-  partner_tag: ENV["AMAZON_ASSOCIATE_TAG"]
-)
-
 ids = [
-  ['B06X9ZNFCW', 20, 'https://amzn.to/38LVj7X', nil],
-  ['B07BL1K1QT', 21, 'https://amzn.to/3pv3iMG', 'https://hb.afl.rakuten.co.jp/ichiba/1dab93b3.956230df.1dab93b4.01b80ab6/_RTLink13434?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbeautyeru%2Flancome_browpodery%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9'],
-  ['B01MR6E3XU', 22, 'https://amzn.to/32QS5fF', nil],
-  ['B07517KSSL', 23, 'https://amzn.to/3lAvT0B', 'https://hb.afl.rakuten.co.jp/ichiba/1dab9491.f4913432.1dab9492.45ae1233/_RTLink13434?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fg-plus%2Fgcha001000%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9']
+  ['B06X9ZNFCW', 20, 'https://amzn.to/38LVj7X', nil, "クラランス ポアパーフェクティングマティファイングキットウィズブロッティングペーパーズ", 5220],
+  ['B07BL1K1QT', 21, 'https://amzn.to/3pv3iMG', 'https://hb.afl.rakuten.co.jp/ichiba/1dab93b3.956230df.1dab93b4.01b80ab6/_RTLink13434?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbeautyeru%2Flancome_browpodery%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9', "ランコム スーシシェイピングクレヨン # 03 Light Brown", 3846],
+  ['B01MR6E3XU', 22, 'https://amzn.to/32QS5fF', nil, "Kylie Cosmetics（カイリーコスメティック）The Burgundy Palette KYSHADOW", 7459],
+  ['B07517KSSL', 23, 'https://amzn.to/3lAvT0B', 'https://hb.afl.rakuten.co.jp/ichiba/1dab9491.f4913432.1dab9492.45ae1233/_RTLink13434?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fg-plus%2Fgcha001000%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9', "シャネル レ ベージュ プードゥル ベル ミン アンソレイエ (ミディアム ライト)", 6970]
 ]
 
 ids.each do |id|
-  response = @client.get_items(item_ids: id[0])
-  items = response.items
-
-  items.each do |item|
-    name = item.title #商品名
-    # url = item.image_url #画像
-    # file = "#{Rails.root}/db/2020amazon_item/#{id[1]}.png"
-    # open(file, 'wb') do |pass|
-    #   open(url) do |recieve|
-    #     pass.write(recieve.read)
-    #   end
-    # end
-
-    price = ""
-    if item.listings.present?
-      item.listings.each do |listing|
-        price = listing.price["Amount"] #価格
-      end
-    else
-      price = nil
-    end
-
-    Item.create!(
-      name: name,
-      image: open("#{Rails.root}/db/2020amazon_item/#{id[1]}.png"),
-      price: price,
-      amazon_url: id[2],
-      rakuten_url: id[3],
-      asin: id[0]
-    ) if Item.find_by(asin: id[0]).blank?
-  end
-
+  Item.create!(
+    name: id[4],
+    image: open("#{Rails.root}/db/2020amazon_item/#{id[1]}.png"),
+    price: id[5],
+    amazon_url: id[2],
+    rakuten_url: id[3],
+    asin: id[0]
+  ) if Item.find_by(asin: id[0]).blank?
 end
