a30c15b86d93d4cc65389cd7fd4255416e26ffc4
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-20 10:40:12 +0900

    title

--- app/controllers/youtubers_controller.rb
+++ app/controllers/youtubers_controller.rb
@@ -4,4 +4,8 @@ class YoutubersController < ApplicationController
     @youtubers = Youtuber.all.order(id: :desc).page(params[:page])
   end
 
+  def show
+    @youtuber = Youtuber.find_by(params[:id])
+  end
+
 end
