b861630073bc89641c25958bb602a9b5d286c09d
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-20 09:16:18 +0900

    動画カテゴリーshow

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,6 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @categories = Category.all
     @items = Item.includes(:post_item_categories).where(post_item_categories: {category_id: params[:id]}).page(params[:page])
+    @posts = Post.includes(:post_item_categories).where(post_item_categories: {category_id: params[:id]}).page(params[:page])
   end
 end
