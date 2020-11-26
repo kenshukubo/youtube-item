134470141b7ce6e69be82d00c43044fc31dfa969
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-26 09:57:31 +0900

    seed

---
+++ db/seeds/2020_11_26_rakuten_item.rb
@@ -0,0 +1,115 @@
+#[rakutenid, item_number, name, price, amazon_url, asin]
+ids = [
+  ['beautyhousehonnpo:10003020', 73, 'スック SUQQU トリートメント プライマー SPF20/PA++', nil, 'https://amzn.to/339gUUv', 'B07X7GDT3M'],
+  ['retailer:10047941', 74, 'スック ザ クリーム ファンデーション 110', nil, 'https://amzn.to/35AVBg6', 'B08H7YGC9H'],
+  ['beautyhousehonnpo:10004084', 75, 'スック ファンデーションブラシ', nil, 'https://amzn.to/36YeLMv', 'B08H8CRQNC'],
+  ['net-pumpkin:10020568', 76, 'スック インテンス カバー コンシーラー 11', nil, 'https://amzn.to/3nuTkJo', 'B08HCF31Y9'],
+  ['blanc-lapin:10168725', 77, 'スック コンフォート リップ フルイド グロウ 01 赤紬 -AKATSUMUGI', nil, 'https://amzn.to/35DoTdV', 'B08H85FT4W'],
+  ['net-pumpkin:10020582', 78, 'スック コンフォート リップ フルイド グロウ 02 結朱 -MUSUBISHU', nil, 'https://amzn.to/2UBN3PQ', 'B08H88WKCN'],
+  ['net-pumpkin:10020583', 79, 'スック コンフォート リップ フルイド グロウ 03 花筏 -HANAIKADA', nil, 'https://amzn.to/3pExipl', 'B08H7X6WZC'],
+  ['net-pumpkin:10020584', 80, 'スック コンフォート リップ フルイド グロウ 04 色暮 -IROGURE', nil, 'https://amzn.to/3lHX1La', 'B08H88CXD8'],
+  ['net-pumpkin:10020585', 81, 'スック コンフォート リップ フルイド グロウ 05 優燈 -YUUHI', nil, 'https://amzn.to/3f5BQjI', 'B08H8J5GPL'],
+  ['net-pumpkin:10020586', 82, 'スック コンフォート リップ フルイド グロウ 06 藤雫 -FUJISHIZUKU', nil, 'https://amzn.to/38L8m9Q', 'B08H8LYMXB'],
+  ['blanc-lapin:10168718', 84, 'スック コンフォート リップ フルイド フォグ 01 小豆飴 -AZUKIAME', nil, 'https://amzn.to/3l3BVG3', 'B08H8N8M24'],
+  ['net-pumpkin:10020575', 85, 'スック コンフォート リップ フルイド フォグ 02 舞紅 -MAIBENI', nil, 'https://amzn.to/3fFohI7', 'B08H81DFJ4'],
+  ['net-pumpkin:10020576', 86, 'スック コンフォート リップ フルイド フォグ 03 綿苺 -WATAICHIGO', nil, 'https://amzn.to/2J7ZNLQ', 'B08H8LYLMC'],
+  ['blanc-lapin:10168721', 87, 'スック コンフォート リップ フルイド フォグ 04 華霜 -HANASHIMO', nil, 'https://amzn.to/3m87tvL', 'B08H7YV834'],
+  ['net-pumpkin:10020578', 88, 'スック コンフォート リップ フルイド フォグ 05 黄熟果 -OUJYUKUKA', nil, 'https://amzn.to/2Hz6iq7', 'B08H84PTJ6'],
+  ['net-pumpkin:10020579', 89, 'スック コンフォート リップ フルイド フォグ 06 絹染 -KINUZOME', nil, 'https://amzn.to/364Vjy7', 'B08H86FYLX'],
+  ['net-pumpkin:10020580', 90, 'スック コンフォート リップ フルイド フォグ 07 焙色 -HOUJIIRO', nil, 'https://amzn.to/3fyBhz7', 'B08H83Z3Y2'],
+  ['naturelab:10005616', 91, 'リッツ うるおい玉 濃縮フリーズドライ コラーゲンボール 6個入り', nil, 'https://amzn.to/362OXPO', 'B0854LJFS7'],
+  ['naturelab:10005799', 92, '【夜用塗るパック】リッツ オーバーナイトセラム 美容液 20g リバイバル', nil, 'https://amzn.to/3l6A0kb', 'B08F5BQ5YD'],
+  ['naturelab:10003147', 93, 'リッツ リバイバル ステム7 (美容クリーム)', nil, 'https://amzn.to/3l7B9b7', 'B06XK81QGB'],
+  ['witch-bazar:10013576', 94, 'ランドリン パフュームハンドクリーム クラシックフローラルの香り 17g', nil, 'https://amzn.to/33jZd4C', 'B08G7B3SRK'],
+  ['grandberry:10001882', 96, 'エレガンス ラ プードル オートニュアンス リクスィーズ VI', nil, 'https://amzn.to/3ldYM1X', 'B00USQ777U'],
+  ['grandberry:10001882', 97, 'ロムアンド rom&nd ベターザンチーク #04 フィグチップ', nil, nil, nil],
+  ['rainmakers-beautyfarm:10000018', 98, 'CipiCipi シピシピ グリッター イルミネーションライナー 01 ピンクベージュ', nil, 'https://amzn.to/3m4Hg1i', 'B08J861XT7'],
+  ['zagzag:10077783', 99, 'セザンヌ 描くふたえアイライナー 影用ブラウン', nil, 'https://amzn.to/33goNHG', 'B07F29BW99'],
+  ['cosmeland:10000795', 100, 'クラランス ダブル フィックスマスカラ', nil, 'https://amzn.to/3l1sC9L', 'B00C5W5S4O'],
+  ['cosmecomonline:10104386', 101, 'ローム・アンド・グラスティングウォーターティント 04', nil, 'https://amzn.to/3nVxmzn', 'B084FCDYZ8'],
+  ['w-import:10023436', 102, 'ディオール アディクト リップ マキシマイザー (020 ブラウン)', nil, 'https://amzn.to/3m57Sit', 'B0876PQZN2'],
+  ['kousuimonogatari:10049991', 103, 'SK-II SK2 スキンパワー エアリー', nil, 'https://amzn.to/3fyPf3Z', 'B08KG7FX1R'],
+  ['freefeel:10042587', 104, 'SK-II スキンパワー クリーム', nil, 'https://amzn.to/2V0XqwB', 'B08LCTJ7FP'],
+  ['freefeel:10042583', 105, 'SK-II SK2 スキンパワー アイクリーム', nil, 'https://amzn.to/2V3Ik9A', 'B08KH1G8GN'],
+  ['freefeel:10042585', 106, 'SK-II スキンパワー エッセンス', nil, 'https://amzn.to/3nSSLcw', 'B08LCT525Q'],
+  ['blanc-lapin:10168110', 107, 'ポール＆ジョー PAUL & JOE リップスティックN レフィル #501 リドー ドゥ セーヌ', nil, nil, nil],
+  ['cosmediva:10005557', 108, 'PAUL & JOE ポール&ジョー リップスティック N(レフィル) #502 ヌヌルス', nil, nil, nil],
+  ['cosmediva:10005558', 109, 'PAUL & JOE ポール&ジョー リップスティック N(レフィル) #503 さんごのイヤリング', nil, nil, nil],
+  ['cosme-venus:10006140', 110, 'PAUL & JOE ポール&ジョー リップスティック N(レフィル) #504 ろうそくの炎', nil, nil, nil],
+  ['g-plus:10029871', 111, 'ポール＆ジョー リップスティック N レフィル 505 ミスチヴァス', nil, nil, nil],
+  ['blanc-lapin:10168106', 112, 'ポール＆ジョー PAUL & JOE リップスティックケースCS #060', nil, nil, nil],
+  ['ab-cos:10006557', 113, 'ポール＆ジョー PAUL&JOEリップスティック ケース CS #061', nil, nil, nil],
+  ['blanc-lapin:10168108', 114, 'ポール＆ジョー PAUL & JOE リップスティックケースCS #062', nil, nil, nil],
+  ['ab-cos:10006071', 116, 'ポール＆ジョー PAUL&JOE リップスティックケースN #02', nil, nil, nil],
+  ['cosmediva:10004168', 119, 'KATE(ケイト) ケイト アイブロウ デザイニングアイブロウ3D ブラウン系 EX-', nil, 'https://amzn.to/2J9s0l2', 'B071XTGPN2'],
+  ['toofaced:10000076', 120, 'Too Faced トゥー フェイスド ラブ ライト ハイライター TooFaced トゥーフェイスド ハイライト レイ オブ ライト', nil, nil, nil],
+  ['cosmecomonline:10104386', 122, 'グラスティングウォーターティント Glasting Water Tint #04 Vintage ocean', nil, 'https://amzn.to/379RmYi', 'B0855YC9JX'],
+  ['zyuuzitsu-life:10002798', 123, 'ナーズ(NARS) ラディアント クリーミー コンシーラー #VANILLA', nil, 'https://amzn.to/3l2AJCN', 'B01CV3FLG6'],
+  ['net-pumpkin:10020536', 124, 'シュウウエムラ アンリミテッドラスティングフルイド SPF 24 - # 474 ライト アプリコット', nil, 'https://amzn.to/375qgl6', 'B086CCJLBR'],
+  ['cosmelink:10055557', 125, 'KATE(ケイト) ケイト アイライナー ダブルラインエキスパート LB-1 極薄ブラウン', nil, 'https://amzn.to/33cX9ew', 'B078JKW7DT'],
+  ['rakuten24:10723417', 126, 'マキアージュ アイブロースタイリング 3D 60 ロゼブラウン (レフィル) 4.2g', nil, 'https://amzn.to/37c1UpO', 'B074CB1DZX'],
+  ['primecollection:10041013', 127, 'コフレドール アイシャドウ ヌーディインプレッションアイズ 01 コーラルブラウン', nil, 'https://amzn.to/3pUOZkR', 'B079RTVRR5'],
+  ['goodcosme2012:10020314', 128, 'クリニーク チークポップ #08', nil, 'https://amzn.to/3fvNfd0', 'B01HTQRGXG'],
+  ['sundrug:10036819', 129, 'キャンメイク シェーディングパウダー01 イエローブラウン', nil, 'https://amzn.to/2KEWXhQ', 'B01MZ1NLAH'],
+  ['lunasol:10000445', 131, 'ルナソル(LUNASOL) プランプメロウリップス 08', nil, 'https://amzn.to/2J74tkK', 'B08CF61QNM'],
+  ['interasia:10000058', 132, 'ドクタージャルト Dr.Jart+ シカペアー リカバー クリーム SPF40/PA++ リニューアル', nil, 'https://amzn.to/3m7zQKq', 'B07Y9Q967H'],
+  ['retailer:10042644', 134, '江原道 KohGenDo (コウゲンドウ) マイファンスィー モイスチャーフィット コンシーラー ミディアム', nil, 'https://amzn.to/3pY04kZ', 'B01M9I2P0Z'],
+  ['partytime-tokyo:10024769', 135, 'RMK マルチペイントカラーズ #04 ピンクジョイ', nil, 'https://amzn.to/3m7IDfp', 'B079DJSP1L'],
+  ['cosmediva:10002495', 136, 'Three by Three 【スリー】シマリング グロー デュオ #01 6.8g ベージュ', nil, 'https://amzn.to/2HB0Cfr', 'B06WV8WC4W'],
+  ['cosmetch:10031707', 138, 'シュウウエムラ・ブロー パレット #ウォルナット ブラウン/エイコーン (アイブローパウダー）', nil, nil, nil],
+  ['kumokumo-square:10015914', 139, 'シュウ ウエムラ アイブローマニキュア ハニーグレイズ', nil, 'https://amzn.to/379kGOM', 'B079VLY3BY'],
+  ['syannsyann:10002641', 140, '【ＲＭＫ チーク】カラー パフォーマンス チークス ＃０１', nil, 'https://amzn.to/3nWOp49', 'B01E6H2LQ8'],
+  ['kumokumo-square:10054127', 141, 'ｓｈｕ ｕｅｍｕｒａ（シュウウエムラ） リキッドルージュ ラック シュプリア ベージュ ＢＧ０２', nil, 'https://amzn.to/364UXHR', 'B07GLHWPSL'],
+  ['flytoyou:10000413', 142, 'ETUDE エチュード プレイカラーアイズ ミューリーロマンス アイシャドウ', nil, 'https://amzn.to/3fx577c', 'B08JCLTJMY'],
+  ['flytoyou:10000412', 143, 'ETUDE エチュード ミューリーロマンス ベターリップトーク ベルベット BE121', nil, 'https://amzn.to/3m5Js8P', 'B08H8912ZN'],
+  ['cosmecomonline:10107560', 144, 'kiss(キス) シマリングクリームアイズ06 アイシャドウ 06 マイシークレット', nil, 'https://amzn.to/2J9mNtJ', 'B08F4QSF5D'],
+  ['cosmecomonline:10107331', 145, 'ミゼルエディ ウォータリーリップティント マット 06ベリーブラウン', nil, 'https://amzn.to/3q3rTID', 'B08HKZVQZH'],
+  ['cosmecomonline:10107332', 146, 'ミゼルエディ ウォータリーリップティント マット 07モカピンク', nil, 'https://amzn.to/373gvE2', 'B08HL13DXY'],
+  ['komahan:10001793', 152, 'INTEGRATE(インテグレート) トリプルレシピアイズ アイシャドウ RD706', nil, 'https://amzn.to/3pY26l7', 'B08DYNQ7Q6'],
+  ['cosmecomonline:10106985', 153, "L'Oreal Paris(ロレアルパリ) カラーリッシュルルージュ N 口紅 107 ヌードブラウン", nil, 'https://amzn.to/2J87zp0', 'B08D3JDZG3'],
+  ['cosmecomonline:10107003', 154, "L'Oreal Paris(ロレアルパリ) カラーリッシュ Mマット Ｎ 口紅 298 ブリックプラネタ", nil, 'https://amzn.to/3q3wYR8', 'B07R3YVSP5'],
+  ['cosmecomonline:10106310', 155, 'CEZANNE セザンヌ ニュアンスオンアイシャドウ02（ローズクォーツ）', nil, 'https://amzn.to/366c9Nb', 'B08HHFMLXK'],
+  ['etvos:10000819', 156, 'ETVOS ミネラルラディアントスキンバーム', nil, 'https://amzn.to/33fHXgN', 'B08HCJ2G68']
+]
+
+ids.each do |id|
+  items = RakutenWebService::Ichiba::Item.search(itemCode: id[0])
+  items.each do |item|
+    name = item.name
+    price = item.price
+    rakuten_url = item.affiliate_url
+    # url = item["mediumImageUrls"][0]
+
+    # file = "#{Rails.root}/db/images/rakuten_api_item/#{id[1]}.png"
+    # open(file, 'wb') do |pass|
+    #   open(url) do |recieve|
+    #     pass.write(recieve.read)
+    #   end
+    # end
+
+    if id[2].present?
+      name = id[2]
+    end
+
+    if id[3].present?
+      price = id[3]
+    end
+
+    if id[5].present?
+      asin = id[5]
+    else
+      asin = nil
+    end
+
+    Item.create!(
+      name: name,
+      item_number: id[1],
+      image: open("#{Rails.root}/db/2020amazon_item/#{id[1]}.png"),
+      price: price,
+      amazon_url: id[4],
+      asin: asin,
+      rakuten_url: rakuten_url,
+      url: id[0]
+    ) if Item.find_by(item_number: id[1]).blank?
+  end
+end
