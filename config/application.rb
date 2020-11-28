d2195d6d01f72c95b54b50ae7a4593c868820c73
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-28 16:26:50 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module YoutubeItem
     config.generators do |g|
       g.helper false
     end
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
