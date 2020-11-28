049dcabbec63b93ab5ae17aa0b5769762492a497
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-28 21:50:01 +0900

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @youtubers = Youtuber.all.order(id: :desc).limit(18)
+    @youtubers = Youtuber.all.order(id: :asc).limit(18)
     @posts = Post.eager_load(:youtuber).order(id: :desc).limit(8)
     @categories = Category.where(ancestry: nil)
     @recentry_update_date = Item.last.created_at.strftime("%m月%d日")
