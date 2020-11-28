1a1dc85d09a5c99749e0cd0a018c90d63e6afc7c
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-11-28 13:57:05 +0900

    seed

---
+++ db/seeds/2020_11_28_rakuten_item.rb
@@ -0,0 +1,127 @@
+#[rakutenid, item_number, name, price, amazon_url, asin]
+ids = [
+  ['cosmecomonline:10077939', 157, 'プリマヴィスタ 皮脂くずれ防止化粧下地 超オイリー肌用 (ブラックプリマ)', 3080, 'https://amzn.to/39k4k8x', 'B084GN18TP'],
+  ['tomods-ap:10032529', 158, 'レブロン カラーステイ クッション ロングウェア ファンデーション 004 バフ（カラーイメージ：明るい肌色 SPF50 PA+++ ）', 2530, 'https://amzn.to/368YzZs', 'B08CSQ8932'],
+  ['grandberry:10001882', 159, 'エレガンス ラ プードル オートニュアンス Ⅵ', nil, nil, 'B015JM1ET4'],
+  # ['foremost:10155863', 160, 'キャンメイク シルキースフレアイズ 05 ライラックモーヴ', nil, 'https://amzn.to/3o2S7cs', 'B08C344R6W'],
+  # ['cosmecomonline:10077904', 161, 'キャンメイク シマリングオーロラアイズ02オーロララベンダー', nil, 'https://amzn.to/3o5rqnJ', 'B07NTTNJW4'],
+  ['beautyplanet-e:10000406', 162, 'shefun メッシュ アイテープ 380枚', nil, 'https://amzn.to/3fEP9rC', 'B07Z39VCB1'],
+  ['cosmecomonline:10053469', 163, 'マキアージュ アイブロースタイリング 3D&ケース 限定セット 60 ロゼブラウン', nil, 'https://amzn.to/2V7Mwp5', 'B0759K9GYX'],
+  ['cosmecomonline:10097633', 164, 'ettusais(エテュセ) アイエディション(ブロウマスカラ) 02 アッシュピンク', 1280, 'https://amzn.to/36cF2Hx', 'B085R8GCWW'],
+  ['cosmecomonline:10046422', 165, 'ヒロインメイクSP ロング&カールマスカラ アドバンストフィルム 02 ブラウン', nil, 'https://amzn.to/3o1HbvF', 'B074TDM8SX'],
+  ['cosmecomonline:10096697', 166, 'excel(エクセル) オーラティックブラッシュAB04 (シャイガール) チーク AB04 シャイガール', 1980, 'https://amzn.to/37cQTVo', 'B084XPQZKC'],
+  ['francecosmeworld:10000104', 167, 'クラランス コンフォート リップオイル インテンス_7g／リップオイル (08 インテンス バーガンディ)', 4950, 'https://amzn.to/2JdXg2K', 'B084C1HL9C'],
+  ['syannsyann:10002435', 168, 'TOO FACED ハイライト ハイライター LOVE LIGHT Blinded by the Light', 5770, 'https://amzn.to/37feiFK', 'B07CGJDTJK'],
+  # ['cosmezakka:10000872', 169, 'V3ファンデーション スピケア SPCARE エキサイティングファンデーション', 7300, 'https://amzn.to/2JexhYX', 'B083CZQ6X4'],
+  ['cosmecomonline:10104247', 170, 'キャンメイク クリームチーク マニキュア 単品 19 シナモンミルクティー', 900, 'https://amzn.to/33mVpj7', 'B08DVCFW4V'],
+  # ['qeen:10620181', 171, 'ナーズ リップスティック/サテン 2910 ソフトローズ', 4880, 'https://amzn.to/2VbuG4i', 'B07XL2NPPX'],
+  # ['qeen:10620183', 172, 'ナーズ リップスティック／サテン 2912 マルドワイン', nil, 'https://amzn.to/37h6YJs', 'B07XV9XGF9'],
+  # ['qeen:10620184', 173, 'ナーズ リップスティック／サテン 2913 ピンクローズ', 4699, 'https://amzn.to/37fJZyy', 'B07XVJQQH6'],
+  # ['ab-cos:10006098', 174, 'イヴ・サンローラン ピュアショット ナイトセラム', nil, 'https://amzn.to/3q3TpWt', 'B085XPHNW9'],
+  ['tomods-ap:10032235', 175, 'ロージーローザ スムースフィットスポンジ', 418, 'https://amzn.to/36cDlKn', 'B085SZY689'],
+  ['cosmecomonline:10103385', 176, "L'Oreal Paris(ロレアルパリ) ブリリアントシグネチャー", 2090, 'https://amzn.to/39mh5iQ', 'B0891BMSPY'],
+  ['blanc-lapin:10146554', 178, 'ヘレナルビンスタイン HELENA RUBINSTEIN リプラスティ R．C． リップ ソーク', 5500, 'https://amzn.to/3fIiEc1', 'B07JKBV1SX'],
+  ['rakuten24:10913180', 179, 'La Roche-Posay(ラロッシュポゼ) 【日やけ止め・化粧下地】UVイデア XL プロテクショントーンアップ SPF50+/PA++++ 30mL ホワイト', nil, 'https://amzn.to/37cSc6K', 'B079X4XGZH'],
+  ['cosmerecipe:10067138', 180, 'クリスチャン ディオール ディオールスキン フォーエヴァー フルイド マット SPF35 - # 1N (ニュートラル)', nil, 'https://amzn.to/3mcxaeQ', 'B07MVJVYKK'],
+  ['soukai:11165957', 181, 'ヴィセ リシェ アイブロウ ペンシル ピンキッシュブラウン BR303', nil, 'https://amzn.to/39j94v5', 'B00TP8O8GK'],
+  ['plazastyle:10013984', 183, 'WHOMEE(フーミー) マルチマスカラ (reddee:レッドブラウン)', 2000, 'https://amzn.to/3fI4Kqy', 'B07HMN9B7V'],
+  ['goodservice:10001932', 184, 'アンリーシア ゲット ジュエル パレット UNLEASHIA Get Jewel Palette #N°2 STARRY DOT', nil, 'https://amzn.to/39mTSNM', 'B08H4WKPVM'],
+  ['cosmecosme:10019205', 186, 'クラランス(CLARINS) マスカラ ダブル フィックス マスカラ', 2507, 'https://amzn.to/33lAyMZ', 'B01M14TLAK'],
+  ['qeen:10617108', 189, 'クリニーク パウダー ポップ フラワー ハイライター 01ルナー ポップ', nil, nil, nil],
+  ['sungrowing:10003214', 190, 'パラドゥ Parado スキンケア クレンジング ミルク メイク落とし クレンジングミルク 240ｇ L サイズ', 2700, 'https://amzn.to/2V8j7eb', 'B07JB8KH84'],
+  ['247online-shop:10013485', 191, 'ParaDo パラドゥ スキンケアクレンジング ミルク状メイク落とし クレンジングミルク 120ｇ', 2000, 'https://amzn.to/3fGEhth', 'B074SDW6TP'],
+  ['monokotoya:10008890', 193, 'メイド・オブ・オーガニクス(made of Organics) ホワイトニングトゥースペイスト シルクパウダー２５ｇ', 880, 'https://amzn.to/36ahUcC', 'B08214LR8L'],
+  # ['momochan-market:10000355', 194, 'FERNANDA(フェルナンダ)Mist & Laundry Soap & Laungry Syrup Gift Set Maria Regale', 2620, 'https://amzn.to/2Jj5HJI', 'B07P6Q47TW'],
+  ['r-kojima:10464470', 195, 'パンテーン 洗い流すトリートメント エクストラダメージケア デイリー補修トリートメント 特大サイズ 300g', 774, 'https://amzn.to/3me50QA', 'B01ID7674Y'],
+  ['kirei-mitsuketa:10009146', 197, 'ドゥーオーガニック クレンジング リキッド', nil, nil, nil],
+  ['matsukiyo:10464081', 198, 'アルジェラン ボディソープ モイスト', 1017, 'https://amzn.to/2JhNdJQ', 'B0728K23D4'],
+  ['ehac:10153720', 199, 'ザ パブリック オーガニック スーパー ポジティブ シャンプー', 1698, 'https://amzn.to/37h8dZ8', 'B07N3Y47TH'],
+  ['cosme-garden:10020655', 200, 'FEMMUE(ファミュ) スーパーファイン モイスチャーミスト[保湿化粧水]', nil, 'https://amzn.to/3nVhUTU', 'B07H7Q5RWR'],
+  ['beautyeru:10035748', 201, 'クリスチャンディオール CHRISTIAN DIOR ディオールスキン フォーエヴァー クッション #1N', 6480, 'https://amzn.to/39mSA5s', 'B07Z9X3N23'],
+  ['brilliant-care:10000275', 202, 'FEMMUE(ファミュ) アイディアルクリーム リッチ', 12980, 'https://amzn.to/2KMM5OX', 'B07ZJQF6QC'],
+  ['guruguru2:12975086', 203, '【レ・メルヴェイユーズ ラデュレ】ローズ リキッド ボディ ウォッシュ', 9999, 'https://amzn.to/2KDFyG8', 'B071LH8RX1'],
+  ['beautitopping:10001093', 204, 'FEMMUE(ファミュ) ルミエール ヴァイタルC <導入美容液>', nil, 'https://amzn.to/3o1Kpzw', 'B07DH4L2L7'],
+  ['sakusaku-d:10098288', 205, '資生堂 クレ・ド・ポー ボーテ ル セラム', 15470, 'https://amzn.to/2JjXncQ', 'B07X9XXNZ9'],
+  ['mugigokoro:10003383', 206, "A'kin (エイキン) エイキン 美容オイルRH 美容液 ローズヒップの香り", nil, 'https://amzn.to/36cCUja', 'B071S5RJ7P'],
+  ['beautifulnet-nagoya:10000859', 207, 'ハホニコ ケラテックス ファイバートリートメント', 3800, 'https://amzn.to/33ibUNh', 'B07SSF9463'],
+  ['beautifulnet-nagoya:10000859', 208, 'ハホニコ ケラテックスファイバーシャンプー', 2980, 'https://amzn.to/3fKyEKH', 'B07SQCPYFG'],
+  ['qeen:10624812', 209, 'サボン バスソルト ハニー ピーチ', 6599, 'https://amzn.to/39knX0g', 'B07D8QT9WW'],
+  ['cosmecomonline:10058500', 210, 'セザンヌ 超細芯アイブロウ 02 オリーブブラウン', 550, 'https://amzn.to/2HJ0BGn', 'B079TNCBN5'],
+  ['tomods-ap:10030347', 211, 'WHOMEE(フーミー) マルチマスカラ (happie:ナチュラルブラウン)', 2500, 'https://amzn.to/2Vb03fi', 'B07HMKVJ5P'],
+  ['qeen:10617988', 212, 'グロス･アンテルディ #01 ローズ･レヴェラトゥール 6ml 【ジバンシイ】', nil, 'https://amzn.to/2VaixN2', 'B0791WZ77D'],
+  ['animalmaru:10001121', 213, 'SUQQU (スック) 2018 15th アニバーサリー カラー コンパクト（限定品） (101 灼熱朱 SHAKUNETSUSHU)', 18999, 'https://amzn.to/3mjLUss', 'B07H4NVDNV'],
+  ['qeen:10617108', 214, 'クリニーク パウダー ポップ フラワー ハイライター 01ルナー ポップ', nil, nil, nil],
+  ['aquabouquet:10064629', 215, 'クラランス コンフォート リップオイル インテンス_7g／リップオイル (01 インテンス ヌード)', 4280, 'https://amzn.to/3m8CQGB', 'B0847KLDK6'],
+  ['beautitopping:10000501', 216, 'rom&nd ロムアンド ジューシーラスティング ティント 10 NUDY PEANUT', nil, 'https://amzn.to/2HHFh3U', 'B08C7N6XSM'],
+  ['aquabouquet:10064633', 217, 'クラランス コンフォート リップオイル インテンス_7g／リップオイル (03 インテンス ラズベリー)', nil, 'https://amzn.to/2VaiBwg', 'B0847K6GMF'],
+  ['aquabouquet:10064632', 218, 'クラランス コンフォート リップオイル インテンス ローズウード (04 ROSEWOOD )', nil, nil, nil],
+  ['aquabouquet:10064631', 219, 'クラランス コンフォート リップオイル インテンス_7g／リップオイル (06 インテンス フューシャピンク)', 3170, 'https://amzn.to/369tKUp', 'B0847KB1SZ'],
+  ['aquabouquet:10064627', 220, 'クラランス コンフォート リップオイル インテンス レッド (07 RED )', nil, nil, nil],
+  ['cinderellabeaute:10031093', 221, 'クラランス ツイスト＆グロー パウダースティック 01 グロー コーラル', nil, nil, nil],
+  ['aquabouquet:10065330', 222, 'クラランス ツイスト＆グロー パウダースティック ラディアント ピンク (02 RADIANT PINK )', nil, nil, nil],
+  ['aquabouquet:10065331', 223, 'クラランス ツイスト＆グロー パウダースティック マンダリン グリーム ( 03 MANDARIN GLEAM )', nil, nil, nil],
+  # ['cinderellabeaute:10031095', 224, 'CLARINS(クラランス)ツイスト リップ デュオ01 (ツイスト リップ ウォーターティント/ツイスト リップ バーム)', nil, nil, nil],
+  # ['sokutei:10000364', 225, 'ポーラ B.A ウォッシュ （旧品） 【洗顔】', nil, 'https://amzn.to/39sIM9I', 'B015C5JTB8'],
+  ['grandberry:10002549', 226, 'ポーラ B.A ライト セレクター 【日焼け止め】', nil, 'https://amzn.to/33mm5QJ', 'B085G253M8'],
+  ['syannsyann:10004022', 227, 'B.A グランラグゼⅢ プレミアムコンセントレートボックス', nil, 'https://amzn.to/3q3lzB0', 'B089F5J1H2'],
+  ['okshop88:10002130', 228, 'ポーラ スパークリングブーケ ボディシャンプーセット', nil, nil, nil],
+  ['shitennoujidou:10405321', 229, 'ルボア(le bois) アンティーム レッグトリートメントキット レッグトリートメントクリーム 200ml & ホワイトクリーム 30g ボディクリーム セット &30g', nil, nil, nil],
+  ['shitennoujidou:10112367', 230, 'クヴォン・デ・ミニム(Le Couvent des Minimes) ボタニカルデュオ アクアサクラエ ボタニカルコロンアクアサクラエ10mL×1、アクアサクラエ ハンドクリーム30g×1', nil, 'https://amzn.to/3me7fmY', 'B07VSB5DNF'],
+  ['cosme-garden:10021603', 231, 'フランシラ Rクローバークレンジングジェル', nil, 'https://amzn.to/3q3lFIS', 'B08LD5DQ4S'],
+  ['cosmeland:10007216', 232, 'クラランス クレーム マスヴェルト', 4890, 'https://amzn.to/3o2Cu4K', 'B00U87RRVK'],
+  ['cosmeland:10007216', 233, 'カネボウ KANEBO グローバル スキン プロテクター', 4690, 'https://amzn.to/2KDH0Z6', 'B07TCSMFPQ'],
+  # ['blanc-lapin:10168537', 234, 'イトリン エレメンタリー エッセンシャルＥＥクリーム_28g／SPF50／PA++++／', 17999, 'https://amzn.to/39fWJYK', 'B08577RWHW'],
+  ['beautyhousehonnpo:10003609', 235, 'Les Merveilleuses LADURÉE（レ・メルヴェイユーズ ラデュレ） ローズ エッセンス デイクリーム_50g／SPF50・PA+++', 7900, 'https://amzn.to/3o1Z91f', 'B0841JK4SM'],
+  # ['grandberry:10002701', 236, 'アユーラ (AYURA) ウォーターフィール UVジェルα SPF50＋ PA＋＋＋＋ 75g', 3533, 'https://amzn.to/3fKAbjV', 'B07Q21RH6S'],
+  ['animalmaru:10001347', 237, 'KANEBO(カネボウ) カネボウ ザ プライマー 化粧下地 エタニティブーケの香り', nil, 'https://amzn.to/36aIHWg', 'B07ZGTPKPV'],
+  ['lunasol:10000372', 238, 'ルナソル(LUNASOL) グロウイングデイクリームUV 美容液', 4950, 'https://amzn.to/3mcmD36', 'B084K5TGK5'],
+  ['broadstage:10032086', 239, 'RMK ボディクリームUV 100g/SPF47 PA+++', 6500, 'https://amzn.to/3mfdOpc', 'B07BLV6RFK'],
+  ['fines-f:10012920', 240, 'ビオレ UV アスリズム スキンプロテクトエッセンス 日焼け止め 70g SPF50+/PA++++', 1164, 'https://amzn.to/36blbbz', 'B07KW3MZJF'],
+  ['rakuten24:10948322', 241, "L'Oreal Paris(ロレアルパリ) ブリリアントシグネチャー リキッド ツヤ 口紅 314 カシスプラム", 1750, 'https://amzn.to/2JkMBCU', 'B0845H8J4M'],
+  ['cosme-off:10004337', 242, 'Dior(ディオール)ディオール アディクト ステラー グロス【976ビー ディオール】', 3680, 'https://amzn.to/3lhGr3H', 'B085JPFGHZ'],
+  ['freefeel:10035411', 243, 'rom&nd ロムアンド ジューシーラスティング ティント JUICY LASTING TINT 09 LITCHI CORAL', 1079, 'https://amzn.to/3mfnBeS', 'B08C729LZP'],
+  # ['ssbiyo:10001296', 244, 'スック トーン タッチ アイズ 12 秋蛍 -AKIHOTARU', 4980, 'https://amzn.to/3le4LU4', 'B07VYTB642'],
+  ['flytoyou:10000354', 245, 'innisfree イニスフリー 選べる３個セット トゥインクル グリッター 2.7g ホログラム 04', nil, nil, nil]
+]
+
+ids.each do |id|
+  items = RakutenWebService::Ichiba::Item.search(itemCode: id[0])
+  items.each do |item|
+    name = item.name
+    price = item.price
+    rakuten_url = item.affiliate_url
+    url = item["mediumImageUrls"][0]
+
+    file = "#{Rails.root}/db/2020amazon_item/#{id[1]}.png"
+    open(file, 'wb') do |pass|
+      open(url) do |recieve|
+        pass.write(recieve.read)
+      end
+    end
+
+    if id[2].present?
+      name = id[2]
+    end
+
+    if id[3].present?
+      price = id[3]
+    end
+
+    if id[5].present?
+      asin = id[5]
+    else
+      asin = nil
+    end
+
+    Item.create!(
+      name: name,
+      item_number: id[1],
+      image: open("#{Rails.root}/db/2020amazon_item/#{id[1]}.png"),
+      price: price,
+      amazon_url: id[4],
+      asin: asin,
+      rakuten_url: rakuten_url,
+      url: id[0]
+    ) if Item.find_by(item_number: id[1]).blank?
+  end
+end
