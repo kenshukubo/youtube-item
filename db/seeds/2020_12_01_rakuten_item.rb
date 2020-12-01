fc8cf36d53ba0b43faf7102253162ccf46b04c6c
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-12-02 00:15:00 +0900

    seed

---
+++ db/seeds/2020_12_01_rakuten_item.rb
@@ -0,0 +1,99 @@
+#[rakutenid, item_number, name, price, amazon_url, asin]
+ids = [
+  ['platinum-online:10001351', 246, 'ZEESEA(ズーシー)（日本語正規品）ダイヤモンドシリーズ 涙マスカラ （銀色ダイヤ）', nil, 'https://amzn.to/3fO8p6k', 'B0861QJPC7'],
+  ['co-beauty:10163187', 247, 'ZEESEA マスカラ スターダイアモンドシリーズ 防水 自然に際立つまつげ 速幹フィルムマスカラ パパッときれい まつ毛をはっきりと分離 カラーマスカラ オレンジジャンム', nil, 'https://amzn.to/37cVBT9', 'B086C29KKF'],
+  ['cosmecomonline:10105553', 249, 'ZEESEA マスカラ スターダイアモンドシリーズ 防水 自然に際立つまつげ 速幹フィルムマスカラ パパッときれい まつ毛をはっきりと分離 カラーマスカラ 抹茶色', nil, 'https://amzn.to/3mg5DZP', 'B086C2H4MX'],
+  ['en-enda:10000120', 253, 'ZEESEA マスカラ スターダイアモンドシリーズ 防水自然に際立つまつげ 速幹フィルムマスカラ パパッときれい まつ毛をはっきりと分離 カラーマスカラ ブラック', nil, 'https://amzn.to/2V8sCtR', 'B086C3B7XT'],
+  ['lanmanya:10000016', 254, 'ZEESEAピカソシリーズアイシャドウ モノクロアイシャドウ 長持ちアイシャドウパレット 芸術デザイン 初心者，S07', 780, 'https://amzn.to/2JkaeM9', 'B0851XSMLQ'],
+  ['lanmanya:10000016', 255, 'ZEESEA(ズーシー)ピカソコラボ アイシャドウパレット （S06）', nil, 'https://amzn.to/3mfz3XV', 'B086V2K6G8'],
+  ['lanmanya:10000016', 256, 'ZEESEA(ズーシー)ピカソコラボ アイシャドウパレット （S08）', nil, 'https://amzn.to/36dX5x3', 'B086V2GKWZ'],
+  ['lanmanya:10000016', 257, 'ZEESEAピカソシリーズアイシャドウ モノクロアイシャドウ 長持ちアイシャドウパレット 芸術デザイン 初心者，G02', nil, 'https://amzn.to/3q2gfOt', 'B0851X9Y7C'],
+  ['lanmanya:10000016', 258, 'ZEESEAピカソシリーズアイシャドウ モノクロアイシャドウ 長持ちアイシャドウパレット 芸術デザイン 初心者，G03', nil, 'https://amzn.to/39jWoEr', 'B08929T8BV'],
+  ['lanmanya:10000016', 259, 'ZEESEAピカソシリーズアイシャドウ モノクロアイシャドウ 長持ちアイシャドウパレット 芸術デザイン 初心者，B12', nil, 'https://amzn.to/2V8nkyv', 'B0851XNLQH'],
+  ['lanmanya:10000016', 260, 'ZEESEA(ズーシー)ピカソコラボ アイシャドウパレット （B13）', nil, 'https://amzn.to/3q9oZlA', 'B086V1JSQ3'],
+  ['cosmeparfaite:10009200', 261, 'アルビオン ホワイト パウダレスト レフィル 10g 010 クールベージュ', nil, 'https://amzn.to/2KMp8vi', 'B085ZVRKFY'],
+  ['ssbiyo:10000344', 262, 'アルビオン スウィート モイスチュア シフォン (レフィル) 10g 6色 SPF22 PA++-ALBION- 010', nil, 'https://amzn.to/3fIF3X2', 'B074J676MB'],
+  ['qulin:10007504', 263, 'Givenchy ジバンシイ タン・クチュール・シマー・パウダー 01シマーピンク', nil, nil, nil],
+  ['qulin:10007504', 264, 'Givenchy ジバンシイ タン・クチュール・シマー・パウダー 02シマーゴールド', nil, nil, nil],
+  ['beautyeru:10035495', 265, 'ナーズ NARS クールクラッシュ アイシャドーパレット【限定品】', nil, nil, nil],
+  ['syannsyann:10001685', 266, 'スック ラディアント クリーム コンシーラー N-01 -SUQQU-', 5885, 'https://amzn.to/2V65hci', 'B07NPFQ51F'],
+  ['qeen:10618380', 267, 'スック センティッドハイドレイティング ミスト WT(60ml)', 5980, 'https://amzn.to/3o2x4He', 'B06Y41MPVR'],
+  ['blanc-lapin:10164712', 268, 'ディオール ディオール アディクト ステラー ハロ シャイン # 669 Superstar', nil, 'https://amzn.to/3mcTtkB', 'B084DKLPD9'],
+  ['aquabouquet:10065198', 269, 'ディオール ディオール アディクト ステラー ハロ シャイン # 536 Lucky Star ※数量限定色', 3548, 'https://amzn.to/36arVXg', 'B085K1SSRV'],
+  ['aquabouquet:10065206', 270, 'ディオール アディクト ステラー ハロ シャイン#667ピンクスター リップスティック', nil, nil, nil],
+  ['aquabouquet:10065204', 271, 'ディオール アディクト ステラー ハロ シャイン 976 ビー ディオール スター', 7599, 'https://amzn.to/2V5Ht8y', 'B086BY1BSW'],
+  ['aquabouquet:10065210', 272, 'ディオール アディクト ステラー ハロ シャイン#740ハッピースター', nil, nil, nil],
+  ['cosme-off:10004328', 273, '【ギフトボックス付き】 【紙袋セット】 ディオール DIOR リップグロス リップバーム アディクト ステラー グロス 6.5ml 553/プリンセス', 6178, 'https://amzn.to/37hZ8Q9', 'B0861YZDH2'],
+  ['qeen:10616794', 274, '【 Dior 】 ディオール アディクト グロス 092 - ステラー', 4300, 'https://amzn.to/33mrEP7', 'B01HTNULG8'],
+  ['cosme-off:10004337', 275, '【ギフトボックス付き】 【紙袋セット】 ディオール DIOR リップグロス リップバーム アディクト ステラー グロス 6.5ml 976/ビーディオール', 6178, 'https://amzn.to/2J5lUCM', 'B0861ZRGGY'],
+  ['diogreco:10004152', 276, 'TOM FORD トムフォード リップ スパーク Lip Spark 02 サージ', nil, nil, nil],
+  ['tokyu-hands:11553446', 277, 'B IDOL(ビーアイドル) B IDOL THE アイパレ 01 本命のブラウン', 1980, 'https://amzn.to/3o4eBdb', 'B084MHRRNS'],
+  ['tokyu-hands:11553447', 278, 'B IDOL(ビーアイドル) B IDOL THE アイパレ 02 駆引きのピンク', 1980, 'https://amzn.to/36c6c1k', 'B084MHMJBM'],
+  ['seiwa-store:10003856', 279, 'B IDOL(ビーアイドル) B IDOL THE アイパレ 03 秘密のオレンジ', 1980, 'https://amzn.to/39kQVwW', 'B084MHVK4G'],
+  ['tokyu-hands:11553443', 280, 'B IDOL(ビーアイドル) B IDOL つやぷるリップ 08 告白PINK', 1540, 'https://amzn.to/2JmuRae', 'B084MHMJ4J'],
+  ['tokyu-hands:11553444', 281, 'B IDOL(ビーアイドル) B IDOL つやぷるリップ 09 無敵テラコッタ', 1540, 'https://amzn.to/2KEjMSC', 'B084MHT41P'],
+  ['tokyu-hands:11553445', 282, 'B IDOL(ビーアイドル) B IDOL つやぷるリップ 10 わがままPLUM', 1540, 'https://amzn.to/3lgEmVK', 'B084MHWSYS'],
+  ['tokyu-hands:11560848', 283, 'B IDOL(ビーアイドル) B IDOL アイブラシ W メイクブラシ', nil, nil, 'B084MHZ77L'],
+  ['viviange:10008078', 284, 'スック SUQQU ピュア カラー ブラッシュ 04 漆陽', 5160, 'https://amzn.to/2V9Np0n', 'B0775FY38P'],
+  ['cosmecomonline:10096698', 285, 'excel(エクセル) オーラティックブラッシュAB05 (ベイクドシナモン) チーク AB05 ベイクドシナモン', nil, 'https://amzn.to/3lg941s', 'B084XQ1RWN'],
+  ['cosmecomonline:10096695', 286, 'excel(エクセル) オーラティックブラッシュAB02 (アプリコットジャム) チーク AB02 アプリコットジャム', 1980, 'https://amzn.to/3mgWyzV', 'B084XPTN5P'],
+  # ['0101marui:11537641', 287, 'レブロン キスグロウ バーム 001 リリーグロウ', nil, nil, nil],
+  ['cosmeland:10025768', 288, 'クリスチャンディオール ディオール アディクト リップ グロウ #001', 4184, 'https://amzn.to/2JeLLbi', 'B002789BAS'],
+  ['0101marui:11495640', 289, 'rom&nd(ロムアンド) 【正規品】JLティント（#13イートドトリ）', 1000, 'https://amzn.to/3o3dFpA', 'B0814SBT3Q'],
+  ['zagzag:10083384', 290, 'ルージュピュールクチュールヴェルニ ウォーターステイン 610 ヌードアンダーウォーター', 5980, 'https://amzn.to/2JiOqR4', 'B07W57VL1X'],
+  ['zagzag:10083385', 291, 'キャンメイク ジューシーピュアアイズ 12 チャイティーローズ アイシャドウ', 660, 'https://amzn.to/2Jmvuk6', 'B0857RYBR9'],
+  ['rakuten24:10950216', 292, 'キャンメイク クイックラッシュカーラー セパレート 02 ブラック', nil, nil, nil],
+  ['yellowshop:16166049', 293, 'キャンメイク クイックラッシュカーラー セパレート 03 ブラウン', nil, nil, nil]
+]
+
+ids.each do |id|
+  items = RakutenWebService::Ichiba::Item.search(itemCode: id[0])
+  items.each do |item|
+    name = item.name
+    price = item.price
+    rakuten_url = item.affiliate_url
+    url = item["mediumImageUrls"][0]
+
+    ##### 本番用 #####
+    # file = "#{Rails.root}/db/2020amazon_item/#{id[1]}.png"
+    # open(file, 'wb') do |pass|
+    #   open(url) do |recieve|
+    #     pass.write(recieve.read)
+    #   end
+    # end
+
+    ##### テスト用 #####
+    # file = "#{Rails.root}/db/images/rakuten_api_item/#{id[1]}.png"
+    # open(file, 'wb') do |pass|
+    #   open(url) do |recieve|
+    #     pass.write(recieve.read)
+    #   end
+    # end
+
+    if id[2].present?
+      name = id[2]
+    end
+
+    if id[3].present?
+      price = id[3]
+    end
+
+    if id[5].present?
+      asin = id[5]
+    else
+      asin = nil
+    end
+
+    Item.create!(
+      name: name,
+      item_number: id[1],
+      image: open("#{Rails.root}/db/2020amazon_item/#{id[1]}.png"), ##### 本番用 #####
+      #image: open("#{Rails.root}/db/images/rakuten_api_item/#{id[1]}.png"), ##### テスト用 #####
+      price: price,
+      amazon_url: id[4],
+      asin: asin,
+      rakuten_url: rakuten_url,
+      url: id[0]
+    ) if Item.find_by(item_number: id[1]).blank?
+  end
+end
