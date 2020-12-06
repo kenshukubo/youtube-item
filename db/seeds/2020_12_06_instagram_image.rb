3f433e41c2bf0a2b8afc302306735d02a7995f24
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-12-06 18:50:01 +0900

    fix

---
+++ db/seeds/2020_12_06_instagram_image.rb
@@ -0,0 +1,10 @@
+date = 20201206
+
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/1.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/2.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/3.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/4.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/5.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/6.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/7.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "uLSE9dJcBVI"), image: open("#{Rails.root}/db/2020instagram/#{date}/8.jpg"))
