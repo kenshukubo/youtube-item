7b254c82c89a44bb3ac0e09075d8ff1f0746b7fc
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-12-07 09:54:19 +0900

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,10 @@ class StaticPagesController < ApplicationController
     @recentry_update_date = Item.last.created_at.strftime("%m月%d日")
     @instagram_post = InstagramImage.where(created_at: Time.current.beginning_of_day..Time.current.end_of_day).first
 
-    items = Item.eager_load(:item_posts).where(item_posts: {post_id: @instagram_post.post.id})
-    @instagram_categories = Category.eager_load(:post_item_categories).where(post_item_categories: {item_id: items.pluck(:id)})
+    if @instagram_post.present?
+      items = Item.eager_load(:item_posts).where(item_posts: {post_id: @instagram_post.post.id})
+      @instagram_categories = Category.eager_load(:post_item_categories).where(post_item_categories: {item_id: items.pluck(:id)})
+    end
   end
 
   def terms
