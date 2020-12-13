c561e9467c23c59b25d50cce0ec0f6b09a8199fd
久保賢周 <kensyu124logic@yahoo.co.jp>
2020-12-13 11:12:50 +0900

    seed

---
+++ db/seeds/2020_12_13_instagram_image.rb
@@ -0,0 +1,10 @@
+date = 20201213
+
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/1.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/2.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/3.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/4.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/5.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/6.jpg"))
+InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/7.jpg"))
+# InstagramImage.create!(post: Post.find_by(video_id: "vTeUxlZapeM"), image: open("#{Rails.root}/db/2020instagram/#{date}/8.jpg"))
